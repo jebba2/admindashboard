e4cd5c191b112c4640932949841efd5efee4bd82
Jeb Barger <jeb@rev-x.com>
2024-02-28 15:56:50 -0500

    Fixes

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import './Navbar.css';
-import { Link } from 'react-router-dom';
-import gsulogo from '../Assets/gsulogo.png';
+import React from "react";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+import gsulogo from "../Assets/gsulogo.png";
 
-function Navbar({onDashboardClick}){
-    return(
-        <nav style={{backgroundColor: '#0033a0'}}>
-            <a href='index.html'>
-            <img src={gsulogo} alt='gsu logo'></img>
-            </a>
-            <div className='name'>CETLOE</div>
-            <div>
-                <ul>
-                    <li>
-                        <Link to={"/"} className='dashboard' onClick={onDashboardClick}>Dashboard</Link>
-                    </li>
-                </ul>
-            </div>
-
-        </nav>
-    );
+function Navbar({ onDashboardClick }) {
+  return (
+    <nav style={{ backgroundColor: "#0033a0" }}>
+      <a href="/admindashboard">
+        <img src={gsulogo} alt="gsu logo"></img>
+      </a>
+      <div className="name">CETLOE</div>
+      <div>
+        <ul>
+          <li>
+            <Link to={"/"} className="dashboard" onClick={onDashboardClick}>
+              Dashboard
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
